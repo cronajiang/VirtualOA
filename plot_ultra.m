bec942fc633628ad438e12fce88f7659ba90b746
Jingjing Jiang <jingjingjiang@Jingjings-MacBook-Pro.local>
2017-06-06 12:29:42 +0200

    change from ultrasound plots to ultraound levels

--- plot_ultra.m
+++ plot_ultra.m
@@ -46,30 +46,58 @@ end
 
 for jj = 1: num_lasers
     for ii = 1:num_wav     
-    laser.z = 0;
-    laser.r = alllasers_r(jj);  
-%     [rl, rb] =  dis_semiinfinite(vessel, laser);
-%      G(jj, ii) = Green_semi(mu_eff_bulk(ii), rl, rb);
-     G(jj, ii) = cal_fluence(mu_eff_bulk(ii),vessel.z, ...
-         sqrt(laser.r.^2 + vessel.z .^2));
-     H(jj, ii) = G(jj,ii) * mua_vessel(ii);
+        laser.z = 0;
+        laser.r = alllasers_r(jj);  
+    %     [rl, rb] =  dis_semiinfinite(vessel, laser);
+    %      G(jj, ii) = Green_semi(mu_eff_bulk(ii), rl, rb);
+         G(jj, ii) = cal_fluence(mu_eff_bulk(ii),vessel.z, ...
+             sqrt(laser.r.^2 + vessel.z .^2));
+         H(jj, ii) = G(jj,ii) * mua_vessel(ii);
     end
+    H_aver(jj) = mean(H(jj,:));
 end
+
+
 %--------------- calculate ultrasound signal level END ----------------%
 %------------------------------ plot START --------------------------- %
 COLORs=hsv(num_lasers);
 % for i=1:num_lasers; plot(1:10,rand(1,10),'col',COLORs(i,:)); end
 % COLORs = jet(num_lasers);
-semilogy(wav, H(1,:)./H(1,1), 'color', [COLORs(1,:)])
-hold on
+% semilogy(wav, H(1,:)./H(1,1), 'color', [COLORs(1,:)])
+ %      
+% semilogy(  H_aver(1)./H_aver(1),'color',[ COLORs(jj,:)],'Marker', '+','MarkerSize',10)
+% hold on
+% str_legend(1) = {['D = ' num2str(sqrt(vessel.z^2 + alllasers_r(1)^2),4)]};
+% if num_lasers > 1
+%     for jj = 2:num_lasers
+% %         semilogy(wav, H(jj,:)./H(1,1),'color',[ COLORs(jj,:)])
+%         semilogy(  H_aver(jj)./H_aver(1),'color',[ COLORs(jj,:)],'Marker', '+','MarkerSize',10)
+%         str_legend{jj} =  ['D = ' num2str(sqrt(vessel.z^2 + alllasers_r(jj)^2),4)];
+%     end
+% end
+
+ 
+      
+    
+      semilogy( [0:2], H_aver(1)./H_aver(1) * ones(1,3),'LineWidth',3,'color',COLORs(1,:))
+   hold on
 str_legend(1) = {['D = ' num2str(sqrt(vessel.z^2 + alllasers_r(1)^2),4)]};
 if num_lasers > 1
     for jj = 2:num_lasers
-        semilogy(wav, H(jj,:)./H(1,1),'color',[ COLORs(jj,:)])
+%         semilogy(wav, H(jj,:)./H(1,1),'color',[ COLORs(jj,:)])
+        semilogy( [0:2], H_aver(jj)./H_aver(1) * ones(1,3),'LineWidth',3,'color',COLORs(jj,:))
         str_legend{jj} =  ['D = ' num2str(sqrt(vessel.z^2 + alllasers_r(jj)^2),4)];
     end
 end
+    
+ Labels = {'Ultrasound Signal'};
+      set(gca, 'XTick', 1, 'XTickLabel', Labels);
+
+%   ymax = 1;
+%       ymin = 0;
+%       ylim([ymin ,ymax])
 legend(str_legend)
-xlabel('wavelength [nm]')
+% xlabel('wavelength [nm]')
 hold off
+
 %------------------------------ plot END ----------------------------- %
